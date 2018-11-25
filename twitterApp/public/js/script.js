0029e6766f11a8d9bd2c622284ef98a8ed8392d5
sid <siddharthnep12@gmail.com>
2018-11-24 18:04:58 -0600

    app works
    yeet yeet

--- twitterApp/public/js/script.js
+++ twitterApp/public/js/script.js
@@ -1,41 +1,51 @@
+
 var inputBox = document.getElementById("us-input");
+var analyzeButton = document.getElementById("analyze-button");
 
-var buttonClicked = false;
+// var show = "<%- positiveTweets %>";
+// console.log(show);
+// if(show == true){
+//     $("#tweet-showcase").hide().slideDown(500);
+//     $(".analysis-results").hide().slideDown(500);
+//     document.getElementById("tweets-from").innerHTML = inputBox.value;
+// }else{
 
-var userEntered = false;
+// }
 
-document.getElementById("analyze-button").addEventListener("click", function () {
-    if (userEntered) {
-        //console.log("cool");
-        changeOtherStuff();
-        buttonClicked = true;
-    }
-});
+// var buttonClicked = false;
 
-var inputLog = function () {
-    verbbose.elementControl();
-}
+// var userEntered = false;
 
-var user;
-inputBox.addEventListener("input", function () {
-    if (inputBox.value != ("")) {
+// document.getElementById("analyze-button").addEventListener("click", function () {
+//     if (userEntered) {
+//         changeOtherStuff();
+//         buttonClicked = true;
+//     }
+// });
 
-        //console.log("yeet");
-        userEntered = true;
+// var inputLog = function () {
+//     verbbose.elementControl();
+// }
 
 
-    }
-    if (inputBox.value == ("") && buttonClicked) {
-        buttonClicked = false;
-        $("#tweet-showcase").hide();
-        $(".analysis-results").hide();
-    }
 
-});
+// var user;
+// inputBox.addEventListener("input", function () {
+//     if (inputBox.value != ("")) {
+//         //console.log("yeet");
+//         userEntered = true;
+//     }
+//     if (inputBox.value == ("") && buttonClicked) {
+//         buttonClicked = false;
+//         $("#tweet-showcase").hide();
+//         $(".analysis-results").hide();
+//     }
 
-function changeOtherStuff() {
+// });
 
-    $("#tweet-showcase").hide().slideDown(500);
-    $(".analysis-results").hide().slideDown(500);
-    document.getElementById("tweets-from").innerHTML = inputBox.value;
-}
+// function changeOtherStuff() {
+
+//     $("#tweet-showcase").hide().slideDown(500);
+//     $(".analysis-results").hide().slideDown(500);
+//     document.getElementById("tweets-from").innerHTML = inputBox.value;
+// }
--- twitterApp/server.js
+++ twitterApp/server.js
@@ -3,7 +3,22 @@ const app = express()
 var path = require('path')
 var bodyParser = require("body-parser");
 
-var module1 = require('./twitter');
+var jsonParser = bodyParser.json();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var twit = require('twit'),
+    twitter = new twit({
+        consumer_key: 'S0g3WkpIJZtxTaa9a6W7l5g73',
+        consumer_secret: 'zvFZWJFqWrO61YRJ9yiR1Y2dtXK3rLOpVCGB44hye7MZUA1lA6',
+        access_token: '2699141594-W0ErGCWuN9sx0e8gOxjZLgh8aKhikFtV8SDJyKu',
+        access_token_secret: 'i06EwY9VSVyoQxHiOPfVnC4yYpuhR34jh1s3zS8g6xUIS'
+    });
+
+var Sentiment = require('sentiment');
+var sentimentA = new Sentiment();
+
+//var tweets = [];
 
 var negativeTweets = "20";
 var positiveTweets = "20";
@@ -14,11 +29,12 @@ var tweet = {
     link: "",
     score: ""
 }
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
@@ -26,22 +42,103 @@ app.get('/', function (req, res) {
         negativeTweets: negativeTweets,
         positiveTweets: positiveTweets,
         neutralTweets: neutralTweets,
-        tweet: tweet.tweet
+        tweet: tweet.tweet,
+        screenName: "",
+        show: false
     });
 })
 
-app.post("/users/screenName", function (req, res) {
+app.post("/users/screenName", urlencodedParser, async function (req, res) {
+    
     tweet.name = req.body.screenName;
     console.log(req.body.screenName);
-    tweetInfo = module1.getTweets(tweet.name);
-
-    if (tweet.name != "") {
-
-        positiveTweets = tweetInfo[0].positive;
-        negativeTweets = tweetInfo[0].negative;
-        neutralTweets = tweetInfo[0].neutral;
-    }
+    var analyzedData = await tweetAnalyzeProcess(tweet.name).then(results => {
+        return results;
+    });
+    console.log(analyzedData.tweetData[1]);
+    res.render('index', {
+        negativeTweets: analyzedData.sentimentData[0].negative,
+        positiveTweets: analyzedData.sentimentData[0].positive,
+        neutralTweets: analyzedData.sentimentData[0].neutral,
+        tweets: analyzedData.tweetData,
+        screenName: tweet.name,
+        show: true
+    })
+    
 })
+
+function getTweets(name){
+    return new Promise(function(resolve, reject){
+
+        twitter.get('statuses/user_timeline', {
+            screen_name: name,
+            count: '200',
+            tweet_mode: 'extended'
+        }, function (error, tweetObj, response) {
+            if(!error){
+                var tweets = [];
+                //var tweetsArray = tweetObj[tweet.length];
+                for (var i = 0; i < tweetObj.length; i++) {
+                    tweets.push({
+                        name: tweetObj[i].user.name,
+                        text: tweetObj[i].full_text,
+                        date_posted: tweetObj[i].created_at,
+                        link: "https://twitter.com/" + tweetObj[i].user.screen_name +"/status/" + tweetObj[i].id_str,
+                        user_link: "https://twitter.com/" + tweetObj[i].user.screen_name,
+                        score: ""
+                    })
+                }
+                
+                resolve(tweets);
+            }
+        })
+    })
+}
+
+function analyzeTweets(tweetsData){
+    return new Promise(function (resolve, reject){
+        
+        for (var i = 0; i < tweetsData.length; i++) {
+            tweetsData[i].score =  sentimentA.analyze(tweetsData[i].text).comparative;
+        }
+        var positive = 0;
+        var negative = 0;
+        var neutral = 0;
+        for (var i = 0; i < tweetsData.length; i++) {
+            if (tweetsData[i].score > 0) {
+                positive++;
+            } else if (tweetsData[i].score < 0) {
+                negative++;
+            } else if (tweetsData[i].score == 0) {
+                neutral++;
+            }
+        }
+    
+        //console.log(positive + " " + negative + " " + neutral);
+        positive = (positive / tweetsData.length) * 100;
+        negative = (negative / tweetsData.length) * 100;
+        neutral = (neutral / tweetsData.length) * 100;
+        //console.log(positive + " " + negative + " " + neutral);
+        console.log("positive:" + positive + "% " + "neutral:" + neutral + "% " + "negative:" + negative + "%");
+        var analyzedResults = [{
+            positive: positive,
+            negative: negative,
+            neutral: neutral
+        }];
+
+        resolve(analyzedResults);
+
+    })
+}
+
+async function tweetAnalyzeProcess(name){
+    var tweetData = await getTweets(name);
+    var sentimentData = await analyzeTweets(tweetData);
+
+    var analyzedData = {tweetData: tweetData, sentimentData: sentimentData};
+    //console.log(entireData);
+    return analyzedData;
+}
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
